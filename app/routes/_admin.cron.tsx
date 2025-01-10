91e149b9be7a7587530022922f769c062c28d483
Magnesium- <yyong007@outlook.com>
2025-01-11 05:11:46 +0800

    feat: add "New Task" button to admin cron page for task creation

--- app/routes/_admin.cron.tsx
+++ app/routes/_admin.cron.tsx
@@ -1,9 +1,10 @@
+import { Link, useLoaderData } from "@remix-run/react";
 import { PageContainer, ProTable } from "@ant-design/pro-components";
 
+import { Button } from "antd";
 import { LoaderFunction } from "@remix-run/node";
 import { createColumn } from "~/components/CronColumns";
 import prisma from "~/lib/prisma";
-import { useLoaderData } from "@remix-run/react";
 
 export const loader: LoaderFunction = async () => {
   const data = await prisma.task.findMany({
@@ -22,7 +23,16 @@ export default function Corn() {
         search={false}
         dataSource={data}
         columns={createColumn()}
+        toolBarRender={() => [<NewTask key="create" />]}
       ></ProTable>
     </PageContainer>
+  );
+}
+
+function NewTask() {
+  return (
+    <Link to="/createCron">
+      <Button type="primary">New Task</Button>
+    </Link>
   );
 }
